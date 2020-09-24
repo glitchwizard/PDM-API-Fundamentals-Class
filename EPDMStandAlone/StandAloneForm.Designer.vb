bb1479354cdfd932bdee23b11b7303771e15378a
glitchwizard <glitchwizard@gmail.com>
2020-09-24 17:47:23 -0600

    added HRESULT button and functionality, which catches version exception and pushes a message box if incompatible (it's incompatible on purpose for this lesson)

--- EPDMStandAlone/StandAloneForm.Designer.vb
+++ EPDMStandAlone/StandAloneForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class StandAloneForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,21 +20,23 @@ Partial Class StandAloneForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.VaultButton = New System.Windows.Forms.Button()
         Me.VaultsComboBox = New System.Windows.Forms.ComboBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.LoginButton = New System.Windows.Forms.Button()
+        Me.HresultButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Button1
+        'VaultButton
         '
-        Me.Button1.Location = New System.Drawing.Point(74, 103)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(148, 73)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Vault"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.VaultButton.Location = New System.Drawing.Point(74, 103)
+        Me.VaultButton.Name = "VaultButton"
+        Me.VaultButton.Size = New System.Drawing.Size(148, 73)
+        Me.VaultButton.TabIndex = 0
+        Me.VaultButton.Text = "Vault"
+        Me.VaultButton.UseVisualStyleBackColor = True
         '
         'VaultsComboBox
         '
@@ -53,14 +55,34 @@ Partial Class StandAloneForm
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Select Preferred Vault View"
         '
+        'LoginButton
+        '
+        Me.LoginButton.Location = New System.Drawing.Point(74, 282)
+        Me.LoginButton.Name = "LoginButton"
+        Me.LoginButton.Size = New System.Drawing.Size(147, 36)
+        Me.LoginButton.TabIndex = 3
+        Me.LoginButton.Text = "Login"
+        Me.LoginButton.UseVisualStyleBackColor = True
+        '
+        'HresultButton
+        '
+        Me.HresultButton.Location = New System.Drawing.Point(71, 324)
+        Me.HresultButton.Name = "HresultButton"
+        Me.HresultButton.Size = New System.Drawing.Size(154, 31)
+        Me.HresultButton.TabIndex = 4
+        Me.HresultButton.Text = "HRESULT"
+        Me.HresultButton.UseVisualStyleBackColor = True
+        '
         'StandAloneForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(286, 450)
+        Me.Controls.Add(Me.HresultButton)
+        Me.Controls.Add(Me.LoginButton)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.VaultsComboBox)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.VaultButton)
         Me.Name = "StandAloneForm"
         Me.Text = "Stand Alone"
         Me.ResumeLayout(False)
@@ -68,7 +90,9 @@ Partial Class StandAloneForm
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents VaultButton As Button
     Friend WithEvents VaultsComboBox As ComboBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents LoginButton As Button
+    Friend WithEvents HresultButton As Button
 End Class
--- EPDMStandAlone/StandAloneForm.vb
+++ EPDMStandAlone/StandAloneForm.vb
@@ -1,6 +1,8 @@
 ﻿Imports EPDM.Interop.epdm
+Imports EPDM.Interop.EPDMResultCode
+
 Public Class StandAloneForm
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles VaultButton.Click
         'Declare an IEdmVault5 interface object
         Dim vault As IEdmVault5
         'Create an instance of IEdmVault5 interface object
@@ -29,5 +31,42 @@ Public Class StandAloneForm
         End If
     End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
+
+        'Declare and create an instance of IEdmVault5 object
+        Dim vault As IEdmVault5 = New EdmVault5()
+
+        'Log in to the selected Vault as the current user
+        vault.LoginAuto(VaultsComboBox.Text, Me.Handle.ToInt32())
+
+        'Check to see if we're logged into a vault
+        MsgBox("We are" + IIf(vault.IsLoggedIn, "", " not") + " logged into a vault.")
+
+        'Display the vault name
+        MsgBox("The vault name is """ + vault.Name + """.")
+
+        'Diplay the path of the vault view folder
+        MsgBox("The path of the vault view folder is """ + vault.RootFolderPath + """.")
+
+    End Sub
+
+    Private Sub Hresult_Click(sender As Object, e As EventArgs) Handles HresultButton.Click
+        'Declare and create an instance of IEdmVault5 object
+        Dim vault As IEdmVault5 = New EdmVault5()
+
+        Try
+            'Verify that the installed version of Solidworks
+            'PDM Pro meets our minimum needs
+            vault.VerifyVersion(29, 0)
+        Catch ex As Runtime.InteropServices.COMException
+            'We will get here if VerifyVersion dosn't return S_OK
+            Select Case ex.ErrorCode
+                Case EdmResultErrorCodes_e.E_EDM_UNSUPPORTED_PROGRAM_VERSION
+                    MessageBox.Show("The installed program is at a version level that is lower than the specified version.")
+                Case Else
+                    MessageBox.Show("HRESULT = 0x" + ex.ErrorCode.ToString("X") + vbCrLf + ex.Message)
+            End Select
+        End Try
+    End Sub
 
 End Class
